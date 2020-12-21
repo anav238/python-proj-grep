40218d7ae3d587f1eb5b387bec8700702ce505ff
anav238 <anavasiliu139@gmail.com>
2020-12-21 14:42:57 +0200

    added recursive file search

--- grep.py
+++ grep.py
@@ -1,18 +1,47 @@
 import sys
 import re
+import os
+
+
+def processFile(file):
+    text_file = open(file, 'r')
+    text = text_file.readlines()
+    matched_lines = []
+    for line in text:
+        line = line.replace("\n", "")
+        match = reg.search(line)
+        if match is not None:
+            matched_lines.append(line)
+
+    if '-count' in options:
+        print(file + " " + len(matched_lines))
+    else:
+        for line in matched_lines:
+            print(file + " " + line)
+
+    text_file.close()
+
+
+def processDirectory(directory):
+    files_and_dirs = os.listdir(directory)
+    for name in files_and_dirs:
+        full_path = directory  + "/" + name
+        if os.path.isdir(full_path):
+            processDirectory(full_path)
+        elif os.path.isfile(full_path):
+            processFile(full_path)
+
 
 if len(sys.argv) < 3:
     raise ValueError("Wrong format! Correct format is: grep.py PATTERNS FILE [OPTION...]")
 
 pattern = sys.argv[1]
+path = sys.argv[2]
 filename = sys.argv[2]
 options = sys.argv[3:]
 pattern = pattern.replace("\"", "")
 if '-v' in options:
     pattern = "^((?!" + pattern + ").)*$"
-
-text_file = open(filename, 'r')
-text = text_file.readlines()
 
 matches = []
 
@@ -21,17 +50,8 @@ if '-ignoreCase' in options:
 else:
     reg = re.compile(pattern)
 
-matched_lines = []
-for line in text:
-    line = line.replace("\n", "")
-    match = reg.search(line)
-    if match is not None:
-        matched_lines.append(line)
+if os.path.isdir(path):
+    processDirectory(path)
+elif os.path.isfile(path):
+    processFile(path)
 
-if '-count' in options:
-    print(len(matched_lines))
-else:
-    for line in matched_lines:
-        print(line)
-
-text_file.close()
